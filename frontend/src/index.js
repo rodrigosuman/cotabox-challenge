ae7fc432bcb4b2d2fd7434da5b6ab4f4f75624af
Rodrigo Suman <rodrigosuman18@gmail.com>
2020-07-12 12:20:05 -0300

    frontend initial commit

---
+++ frontend/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <React.Fragment>
+    <h1>Cotabox's challenge frontend</h1>
+  </React.Fragment>,
+  document.getElementById("root")
+);
