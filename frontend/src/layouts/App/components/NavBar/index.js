38802f618f54ddd74164596ca4f23b0db0aa8c51
Rodrigo Suman <rodrigosuman18@gmail.com>
2020-07-12 17:16:28 -0300

    feature (frontend): app navbar

---
+++ frontend/src/layouts/App/components/NavBar/index.js
@@ -0,0 +1,98 @@
+import React, { useRef, useState } from "react";
+
+import * as Yup from "yup";
+
+import { Container, UserName, FormContent, SubmitButton } from "./styles";
+
+import { useAuth } from "../../../../contexts/auth.context";
+
+import { useUnform } from "../../../../modules/unform.module";
+
+import { createPeopleDocument } from "../../../../services/people.service";
+
+import { CircularProgress } from "@material-ui/core";
+
+function NavBar() {
+  const { user, signOut } = useAuth();
+
+  const { Form, Input } = useUnform();
+
+  const formRef = useRef(null);
+
+  const [submiting, toggleSubmiting] = useState(false);
+
+  return (
+    <Container>
+      <UserName>
+        {user.name}
+        <br />
+        <strong onClick={() => signOut()}>Logout</strong>
+      </UserName>
+      <Form ref={formRef} onSubmit={handleSubmit}>
+        <FormContent>
+          <Input
+            customStyles={{ marginBottom: 0, marginRight: 16, width: 256 }}
+            name="first_name"
+            label="First name"
+          />
+          <Input
+            customStyles={{ marginBottom: 0, marginRight: 16, width: 256 }}
+            name="last_name"
+            label="Last name"
+          />
+          <Input
+            customStyles={{ marginBottom: 0, marginRight: 16, width: 256 }}
+            name="participation"
+            label="Participation"
+            type="number"
+          />
+          <SubmitButton type="submit">
+            {submiting ? (
+              <CircularProgress color="inherit" size={18} />
+            ) : (
+              "Send"
+            )}
+          </SubmitButton>
+        </FormContent>
+      </Form>
+    </Container>
+  );
+
+  async function handleSubmit(data) {
+    console.log(data);
+    try {
+      toggleSubmiting(true);
+
+      // Remove all previous errors
+      formRef.current.setErrors({});
+      const schema = Yup.object().shape({
+        first_name: Yup.string().required(),
+        last_name: Yup.string().required(),
+        participation: Yup.number().required(),
+      });
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+      // Validation passed
+      createPeopleDocument({
+        ...data,
+        participation: Number(data.participation),
+      })
+        .then((res) => console.log(res.data))
+        .catch((err) => console.log(err.response));
+
+      toggleSubmiting(false);
+    } catch (err) {
+      toggleSubmiting(false);
+      const validationErrors = {};
+      if (err instanceof Yup.ValidationError) {
+        err.inner.forEach((error) => {
+          validationErrors[error.path] = error.message;
+        });
+        formRef.current.setErrors(validationErrors);
+      }
+    }
+  }
+}
+
+export default NavBar;
---
+++ frontend/src/layouts/App/components/NavBar/styles.js
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+
+export const Container = styled.nav`
+  background: #ffffff21;
+  margin-bottom: 48px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 32px 48px;
+`;
+
+export const UserName = styled.p`
+  color: #21ffa4;
+  font-weight: 600;
+  text-align: right;
+  line-height: 24px;
+  align-self: flex-end;
+  margin-bottom: 32px;
+
+  strong {
+    color: #ffffff59;
+    font-size: 14px;
+    cursor: pointer;
+
+    &:hover {
+      color: #ffffff2e;
+    }
+  }
+`;
+
+export const FormContent = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const SubmitButton = styled.button`
+  width: 176px;
+  height: 49px;
+  font-size: 18px;
+  color: #fff;
+  background: #6f2fbf;
+  padding: 12px;
+  font-weight: 600;
+  border-radius: 4px;
+  cursor: pointer;
+
+  margin-top: 21px;
+
+  &:hover {
+    background: #8316e275;
+  }
+`;
