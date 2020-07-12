b767f77f48e044d501dcf5a304b416f99891926b
Rodrigo Suman <rodrigosuman18@gmail.com>
2020-07-12 20:05:14 -0300

    feature (frontend): delete a person document

--- frontend/src/pages/App/components/PeopleList/index.js
+++ frontend/src/pages/App/components/PeopleList/index.js
@@ -1,8 +1,16 @@
 import React from "react";
 
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 import { Table } from "./styles";
+
+import { PieChart } from "react-minimal-pie-chart";
+
+import { Delete } from "@material-ui/icons";
+
+import { deletePersonDocument } from "../../../../services/people.service";
+
+import { setPeopleListAction } from "../../../../redux/ducks/People";
 
 function PeopleList() {
   const { data } = useSelector(({ peopleReducer }) => peopleReducer);
@@ -14,18 +22,50 @@ function PeopleList() {
           <th>First name</th>
           <th>Last name</th>
           <th>Participation</th>
+          <th></th>
+          <th></th>
         </tr>
 
         {data.map((item, key) => (
           <tr key={key}>
             <td>{item.first_name}</td>
             <td>{item.last_name}</td>
-            <td>{item.participation}%</td>
+            <td>{item.value}%</td>
+            <td>
+              <Delete
+                style={{ cursor: "pointer" }}
+                onClick={() => handleDelete(item.id)}
+              />
+            </td>
+            <td>
+              <div
+                style={{
+                  width: 16,
+                  height: 16,
+                  borderRadius: 8,
+                  background: item.color,
+                }}
+              />
+            </td>
           </tr>
         ))}
       </Table>
     </div>
   );
+
+  function handleDelete(id) {
+    deletePersonDocument(id).then(() => {
+      const newPersonList = data
+        .map((item) => {
+          if (item._id !== id) {
+            return item;
+          }
+        })
+        .filter((item) => item !== undefined);
+
+      dispatch(setPeopleListAction(newPersonList));
+    });
+  }
 }
 
 export default PeopleList;
--- frontend/src/services/people.service.js
+++ frontend/src/services/people.service.js
@@ -3,3 +3,5 @@ import api from "./api.service";
 export const getAllPeopleData = () => api.get("/people");
 
 export const createPeopleDocument = (body) => api.post("/people", body);
+
+export const deletePersonDocument = (id) => api.delete(`/people/${id}`);
